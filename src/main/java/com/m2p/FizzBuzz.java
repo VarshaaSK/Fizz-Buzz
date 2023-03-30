57aa9f67ac65cd8b08be00a6997475a97ba2f488
VarshaaSK <varshaa.sk@m2pfintech.com>
2023-03-30 10:10:02 +0530

    Req 1 : Logic and Test Case added to Return FizzBuzz

--- src/main/java/com/m2p/FizzBuzz.java
+++ src/main/java/com/m2p/FizzBuzz.java
@@ -1,4 +1,44 @@
 package com.m2p;
 
 public class FizzBuzz {
+
+    private int number;
+    FizzBuzz(int number){
+        this.number = number;
+    }
+
+    public boolean validityCheck(){
+        if(number >= 1 && number <= 100){
+            return true;
+        }
+        return false;
+    }
+
+
+    public String multipleOfThree() {
+        if(validityCheck()){
+            if(number % 3 == 0){
+                return "Fizz";
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public String multipleOfFive() {
+        if(validityCheck()){
+            if(number % 5 == 0){
+                return "Buzz";
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public String notMultipleOfAny() {
+        if(validityCheck()){
+            if(number % 3 != 0 && number % 5 != 0){
+                return Integer.toString(number);
+            }
+        }
+        throw new IllegalArgumentException();
+    }
 }
--- src/test/java/com/m2p/FizzBuzzTest.java
+++ src/test/java/com/m2p/FizzBuzzTest.java
@@ -1,5 +1,56 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class FizzBuzzTest {
-    FizzBuzz fizzbuzz = new FizzBuzz();
+
+    FizzBuzz buzz = new FizzBuzz(15);
+    FizzBuzz fizz = new FizzBuzz(15);
+    FizzBuzz numberAsString = new FizzBuzz(15);
+    @Test
+    void toReturnFizz(){
+
+
+
+        ///Arrange
+        String expectedString = "Fizz";
+
+        //Act
+        String actualString = fizz.multipleOfThree();
+
+        //Assert
+        assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    void toReturnBuzz(){
+
+
+        //Arrange
+        String expectedString = "Buzz";
+
+        //Act
+        String actualString = buzz.multipleOfFive();
+
+        //Assert
+        assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    void toReturnNumber(){
+
+        //Arrange
+        String expectedString = "15";
+
+        //Act
+        String actualstring = numberAsString.notMultipleOfAny();
+
+        //Assert
+        assertEquals(expectedString, actualstring);
+    }
+
+
 }
